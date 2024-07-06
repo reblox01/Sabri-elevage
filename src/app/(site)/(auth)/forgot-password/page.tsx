a99892c3145197c186748d879e3083d65fae6677
0x8D <mirocairo15@gmail.com>
2024-07-06 23:31:50 +0100

    init

---
+++ src/app/(site)/(auth)/forgot-password/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ForgotPassword from "@/components/Auth/ForgotPassword";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Forgot Password | Sabri elevage",
+};
+
+const ForgotPasswordPage = () => {
+  return (
+    <>
+      <Breadcrumb pageName="Forget Password" />
+      <ForgotPassword />
+    </>
+  );
+};
+
+export default ForgotPasswordPage;
---
+++ src/app/(site)/about/page.tsx
@@ -0,0 +1,22 @@
+import About from "@/components/About";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Team from "@/components/Team";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "About Us | Sabri elevage",
+  description: "This is About page description",
+};
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Breadcrumb pageName="About Us Page" />
+      <About />
+      <Team />
+    </main>
+  );
+};
+
+export default AboutPage;
