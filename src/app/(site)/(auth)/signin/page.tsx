fd1fd33037a57d295c8dabe48a5d826e7a397e68
0x8D <mirocairo15@gmail.com>
2024-07-07 16:56:36 +0100

    update metadata

--- src/app/(site)/(auth)/signin/page.tsx
+++ src/app/(site)/(auth)/signin/page.tsx
@@ -3,8 +3,10 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "Se Connecter | Sabri Élevage",
+  title: "Se Connecter | Sabri Élevage",
+  description: "Connectez-vous à Sabri Élevage pour gérer votre compte et accéder à nos services spécialisés en élevage de poulets vivants de qualité.",
+  keywords: "Sabri Élevage, connexion, élevage de poulets, qualité, bien-être animal, agriculture",
+  robots: "noindex, nofollow",
 };
 
 const SigninPage = () => {
--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -4,9 +4,10 @@ import Team from "@/components/Team";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "À propos | Sabri Élevage",
-  description: "This is À propos page description",
+  title: "À propos - Sabri Élevage",
+  description: "Découvrez Sabri Élevage, spécialiste en élevage de poulets vivants de qualité, avec un engagement fort envers le bien-être animal et la durabilité. Nous sommes dédiés à fournir des produits de qualité tout en respectant les normes les plus élevées de l'industrie.",
+  keywords: "À propos de Sabri Élevage, élevage de poulets vivants, qualité, bien-être animal, durabilité, agriculture",
+  robots: "index, follow",
 };
 
 const AboutPage = () => {
