51672169e81fe4d1057a383142cb5c24b5dd52e8
0x8D <mirocairo15@gmail.com>
2024-07-07 23:32:24 +0100

    complete reviews

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -4,31 +4,28 @@ import SingleTestimonial from "./SingleTestimonial";
 
 const testimonialData: Testimonial[] = [
   {
-    id: 1,
-    name: "Sabo Masties",
-    designation: "Founder @ Rolex",
-    content:
-      "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
-    image: "/images/testimonials/author-01.png",
-    star: 5,
+    "id": 1,
+    "name": "Khalid Benjelloun",
+    "designation": "Client @ Casablanca",
+    "content": "Je suis extrêmement satisfait des services de Sabri Elevage. Le personnel est très professionnel et les produits sont de haute qualité.",
+    "image": "/images/testimonials/author-01.png",
+    "star": 5
   },
   {
-    id: 2,
-    name: "Margin Gesmu",
-    designation: "Founder @ UI Hunter",
-    content:
-      "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
-    image: "/images/testimonials/author-02.png",
-    star: 5,
+    "id": 2,
+    "name": "Amina El Amrani",
+    "designation": "Client @ Rabat",
+    "content": "L'expérience avec Sabri Elevage a été exceptionnelle. Leur site web est facile à naviguer et les délais de livraison sont respectés.",
+    "image": "/images/testimonials/author-02.png",
+    "star": 4
   },
   {
-    id: 3,
-    name: "William Smith",
-    designation: "Founder @ Trorex",
-    content:
-      "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
-    image: "/images/testimonials/author-03.png",
-    star: 5,
+    "id": 3,
+    "name": "Youssef Aït Ben Haddou",
+    "designation": "Seller @ Marrakech",
+    "content": "En tant que vendeur, collaborer avec Sabri Elevage a été une excellente décision. Leur plateforme est conviviale et ils offrent un excellent support.",
+    "image": "/images/testimonials/author-03.png",
+    "star": 5
   },
 ];
 
@@ -40,8 +37,9 @@ const Testimonials = () => {
           // subtitle="Testimonials"
           title="Ce que Disent nos Clients"
           
-          paragraph="There are many variations of passages of Lorem
-           Ipsum available but the majority have suffered alteration in some form."
+          paragraph="Découvrez les témoignages de nos clients satisfaits. 
+            Leur satisfaction est notre priorité et nous sommes fiers de partager 
+            leurs expériences positives avec Sabri Elevage."
            width="640px"
           center
           
