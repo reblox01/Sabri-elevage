9c02db03f0d846c159ad7a6f04d116ae1fb83240
akram ahil <akram.ahil123@gmail.com>
2024-07-08 14:50:43 +0100

    testt

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -103,13 +103,16 @@ const Header = () => {
               </Link>
             </div>
             <div className="flex w-full items-center justify-between px-4">
+              
               <div>
+                
                 <button
                   onClick={navbarToggleHandler}
                   id="navbarToggler"
                   aria-label="Mobile Menu"
                   className="absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
                 >
+                  
                   <span
                     className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
                       navbarOpen ? " top-[7px] rotate-45" : " "
@@ -119,6 +122,7 @@ const Header = () => {
                         : "bg-white"
                     }`}
                   />
+                  
                   <span
                     className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
                       navbarOpen ? "opacity-0 " : " "
