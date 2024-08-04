1a65490a4c24e12784e5b124fe2717b50f9f859a
0x8D <mirocairo15@gmail.com>
2024-08-04 21:59:09 +0100

    update iframe

--- src/components/Admin/Tables/Poulettes/index.tsx
+++ src/components/Admin/Tables/Poulettes/index.tsx
@@ -23,10 +23,8 @@ export default async function IndexPage({
     <main className="flex flex-1 flex-col p-4 md:p-20 mt-20">
       <div className="flex items-center mb-8">
         <h1 className="font-semibold text-lg md:text-2xl">
-          La Table des Poules | 
-          <span className="text-2xl underline underline-offset-4 hover:font-bold decoration-wavy decoration-sky-500">
-            Mostafa PC
-          </span>
+          La Table des Poules |
+          <span className="text-2xl underline underline-offset-4 hover:font-bold decoration-wavy decoration-sky-500"> Mostafa PC</span>
         </h1>
       </div>
       <div className='flex items-center mb-10'>
